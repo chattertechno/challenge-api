02e7b9c4cae73d51f01970076bda6b21372c3b3c
chattertechno <reneekrom28@gmail.com>
2022-12-26 17:00:38 +0300

    UPDATE DB CONNECTION

--- db/db.go
+++ db/db.go
@@ -17,15 +17,15 @@ func Dbconnect() *mongo.Client {
 	clientOptions := options.Client().ApplyURI(middlewares.DotEnvVariable("MONGO_URL"))
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		log.Fatal("⛒ Connection Failed to Database")
+		log.Fatal("⛒ Connection Failed to Database 1")
 		log.Fatal(err)
 	}
 	// Check the connection
 	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		log.Fatal("⛒ Connection Failed to Database")
-		log.Fatal(err)
-	}
+	// if err != nil {
+	// 	log.Fatal("⛒ Connection Failed to Database 2")
+	// 	log.Fatal(err)
+	// }
 	color.Green("⛁ Connected to Database")
 	return client
 }
